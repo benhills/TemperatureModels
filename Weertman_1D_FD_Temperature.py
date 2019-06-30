22b33df906b8424dadb23473afaae5774313da57
Benjamin Hills <benjaminhhills@gmail.com>
2019-06-30 12:18:56 -0700

    Fix bug for refreezing.
    Freezing was taking the temperature from the top of the profile instead
    of the bottom.

--- Weertman_1D_FD_Temperature.py
+++ Weertman_1D_FD_Temperature.py
@@ -232,8 +232,8 @@ def Weertman_T(Ts,qgeo,H,adot,const,dTs=[0.,0.],dH=[0.,0.],da=[0.,0.],v_surf=[0.
                     Mrate = np.append(Mrate,Tplus*const.rho*const.Cp*const.spy/(const.rhow*const.L*dt))
             # If freezing
             elif Mcum[-1] > 0 and melt:
-                Tminus = (T[-1]-PMP[-1])*0.5*dz
-                T[-1] = PMP[-1]
+                Tminus = (T[0]-PMP[0])*0.5*dz
+                T[0] = PMP[0]
                 if i%100 == 0:
                     Mrate = np.append(Mrate,Tminus*const.rho*const.Cp*const.spy/(const.rhow*const.L*dt))
             elif melt and i%100 == 0:
